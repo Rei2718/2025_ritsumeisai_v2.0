08bcda2e2b17edfdfb92adc4e60dd6eeae901b22
Rei2718 <once2718@gmail.com>
2025-07-06 11:39:51 +0900

    d

--- components/food/FoodClient.tsx
+++ components/food/FoodClient.tsx
@@ -126,7 +126,7 @@ export default function FoodClient({ vendors }: FoodClientProps) {
                 }}
               >
                   <p className="text-[var(--text-tertiary)] text-center">
-                    やや混雑中
+                    混雑中
                   </p>
               </motion.div>
             </section>
